22211f9998e23820b1f0bbee9cabb8c288e8d5b1
John Tran <j.tran03@outlook.com>
2022-06-12 18:00:08 +1000

    hh

--- ROS Packages/zlac8015d/scripts/talker.py
+++ ROS Packages/zlac8015d/scripts/talker.py
@@ -377,10 +377,13 @@ def callback(msg):
     # rospy.loginfo("Angular Components: [%f]"%(msg.angular.z))
     
     # inverse kinematics
-    left_rpm = (msg.linear.x + WHEEL_DISTANCE*msg.angular.z)/WHEEL_RADIUS
-    right_rpm = -(msg.linear.x - WHEEL_DISTANCE*msg.angular.z)/WHEEL_RADIUS
+    print(msg.linear.x)
+
+	print("Hello to the %d %d " %(msg.linear.x, msg.angular.z))
+	# left_rpm = (msg.linear.x + WHEEL_DISTANCE*msg.angular.z)/WHEEL_RADIUS
+    # right_rpm = -(msg.linear.x - WHEEL_DISTANCE*msg.angular.z)/WHEEL_RADIUS
     
-    motors.set_rpm(int(left_rpm),int(right_rpm))
+    # motors.set_rpm(int(left_rpm),int(right_rpm))
     
 
 def listener():
