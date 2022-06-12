acc5a84f7ff1d247cbb8dc195c1585d4b644b6d7
jtran03 <60172315+jtran03@users.noreply.github.com>
2022-06-13 04:06:13 +1000

    bing bong

--- Drivers/ZLAC8015D/test_speed_control.py
+++ Drivers/ZLAC8015D/test_speed_control.py
@@ -18,10 +18,10 @@ motors.enable_motor()
 cmds = [1, -1]
 
 motors.set_rpm(cmds[0],cmds[1])
-
-start_time = time.time()
 f = open("demofile2.txt", "w")
 
+# First loop #######################
+start_time = time.time()
 while time.time() - start_time < 10:
 	try:
 		dL, dR, enL, enR = motors.get_wheels_travelled()
@@ -31,7 +31,20 @@ while time.time() - start_time < 10:
 	except KeyboardInterrupt:
 		motors.disable_motor()
 		break
+# Wait 5 seconds#######################
+time.sleep(5)
+# Second loop #######################
+start_time = time.time()
+while time.time() - start_time < 10:
+	try:
+		dL, dR, enL, enR = motors.get_wheels_travelled()
 
+		f.write(str(time.time()) + ", " + str(enL) + ", " + str(enR) + "\n")
+		#time.sleep(1)
+	except KeyboardInterrupt:
+		motors.disable_motor()
+		break
+###############################
 f.close()
 motors.disable_motor()
 
