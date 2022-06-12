d2be88b91c99e4574ffbbb597e0885d28b76b961
John Tran <j.tran03@outlook.com>
2022-06-12 18:24:05 +1000

    j

--- ROS Packages/zlac8015d/scripts/talker.py
+++ ROS Packages/zlac8015d/scripts/talker.py
@@ -381,8 +381,7 @@ def callback(msg):
 		left_rpm = (msg.linear.x + WHEEL_DISTANCE*msg.angular.z)/WHEEL_RADIUS
     	right_rpm = -(msg.linear.x - WHEEL_DISTANCE*msg.angular.z)/WHEEL_RADIUS
     	motors.set_rpm(int(left_rpm),int(right_rpm))
-	
-    old_msg = msg
+	old_msg = msg
 
 
 def listener():
