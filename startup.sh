1d62a4b49270ed341893b7d56cd9c2d6edbbdf94
jtran03 <60172315+jtran03@users.noreply.github.com>
2022-06-19 17:14:32 +1000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-gnome-terminal -x bash -c "roslaunch zed_wrapper zed2.launch"
+gnome-terminal -- bash -c "roslaunch zed_wrapper zed2.launch"
 
 
 #gnome-terminal -- "source /opt/ros/melodic/setup.bash; source ~/catkin_ws/devel/setup.bash;" 
