80244039aed4dd29d7bfb7ad8cf9294e10c26aa3
jtran03 <60172315+jtran03@users.noreply.github.com>
2022-06-19 18:32:20 +1000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
+# ZED 2
 gnome-terminal -- bash -c "roslaunch zed_wrapper zed2.launch"
 
-sleep 5
+sleep 7
 
+# IMU 
 gnome-terminal -- bash -c "roslaunch tinkerforge_imu_ros bricks_v3.launch"
 
+# Web Page
 gnome-terminal -- bash -c "roslaunch rosbridge_server rosbridge_websocket.launch"
 
+# Motor Driver
 gnome-terminal -- bash -c "rosrun zlac8015d talker.py"
 
-gnome-terminal -- bash -c "~/Desktop/VirtualEnv/start_nodejs.sh"
+# RPLiDAR
+gnome-terminal -- bash -c "sshpass -p "1324" ssh -o StrictHostKeyChecking=no jhtran@10.42.0.212 'bash -is < ~/Desktop/startup.sh'"
 
+# Node JS
+gnome-terminal -- bash -c "~/Desktop/VirtualEnv/start_nodejs.sh
 #gnome-terminal -- "source /opt/ros/melodic/setup.bash; source ~/catkin_ws/devel/setup.bash;" 
