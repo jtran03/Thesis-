95b23b0990c606fc16d16252b942b4d7cb0b53e8
jtran03 <60172315+jtran03@users.noreply.github.com>
2022-06-19 17:15:58 +1000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,5 +2,11 @@
 
 gnome-terminal -- bash -c "roslaunch zed_wrapper zed2.launch"
 
+gnome-terminal -- bash -c "roslaunch tinkerforge_imu_ros bricks_v3.launch"
+
+gnome-terminal -- bash -c "roslaunch rosbridge_server rosbridge_websocket.launch"
+
+gnome-terminal -- bash -c "rosrun zlac8015d talker.py"
+
 
 #gnome-terminal -- "source /opt/ros/melodic/setup.bash; source ~/catkin_ws/devel/setup.bash;" 
