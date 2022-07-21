dbba138940484496a2bf0cd48a1ddbee5662b36c
jtran03 <60172315+jtran03@users.noreply.github.com>
2022-07-21 16:33:45 +1000

    Update startup2.sh

--- startup2.sh
+++ startup2.sh
@@ -2,6 +2,5 @@
 
 gnome-terminal -- bash -c "roslaunch zlac8015d_motor_driver full.launch"
 sleep 7
-gnome-terminal -- bash -c "sshpass -p "1324" ssh -o StrictHostKeyChecking=no jhtran@10.0.0.3 'bash -is < ~/Desktop/startup.sh'"
 gnome-terminal -- bash -c "~/Desktop/VirtualEnv/start_nodejs.sh"
-rosrun rviz rviz -d ~/Desktop/test.rviz
+sshpass -p "1324" ssh -o StrictHostKeyChecking=no jhtran@10.0.0.3 'bash -is < ~/Desktop/startup.sh'
