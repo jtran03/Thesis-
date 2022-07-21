1f9c38769cbcd98abe081047c63ee3de66da5231
jtran03 <60172315+jtran03@users.noreply.github.com>
2022-07-21 16:44:48 +1000

    Update startup2.sh

--- startup2.sh
+++ startup2.sh
@@ -5,9 +5,3 @@ sleep 7
 gnome-terminal -- bash -c "~/Desktop/VirtualEnv/start_nodejs.sh"
 
 ssh jhtran@10.0.0.3 
-spawn ssh jhtran@10.0.0.3 
-expect "Password:*"
-send "1324\r"
-expect "$ "
-interact
-~/Desktop/startup.sh
