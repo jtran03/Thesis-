07557da4c265a6daf5a1caa029d2c99f4c0d8c86
jtran03 <60172315+jtran03@users.noreply.github.com>
2022-07-28 22:13:24 +1000

    dwdw

--- Control Algorithms/Navigation_Stack/myFunction.m
+++
@@ -1,8 +0,0 @@
-function myFunction(data)
-    
-    pub = rospublisher('/zlac8015d/rpm','std_msgs/Float32MultiArray');
-    msg = rosmessage(pub);
-    msg.Data = [data(1,1), data(2,1)];
-    send(pub,msg);
-    
-end 
