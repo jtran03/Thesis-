b6cfbd8dabd66d4b0068e1331142b377d3354ba2
jtran03 <60172315+jtran03@users.noreply.github.com>
2022-07-29 00:30:13 +1000

    lol

--- Control Algorithms/Navigation_Stack/start.m
+++ Control Algorithms/Navigation_Stack/start.m
@@ -1,5 +1,13 @@
 wheelBase = 0.4464; %m
 wheelRadius = 0.1715/2; %m
-timeStep = 0.1; %s (setting a default time step) 
+timeStep = 0.1; % s or 10Hz  (setting a default time step) 
+
+% Kalman Filter 
+initialPose = [0, 0, 0]; 
+initialErrorCovariance = [0.1 0 0; 0 0.1 0; 0 0 0.1]; 
+Q = [0.1 0 0; 0 0.1 0; 0 0 0.1]; 
+R = [0.5 0 0; 0 0.5 0; 0 0 0.5]; 
+
+
 
 open_system('navigation.slx')
