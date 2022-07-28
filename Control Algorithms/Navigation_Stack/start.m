54153c437150ef77c5cd0fa50fa5798df73d10de
jtran03 <60172315+jtran03@users.noreply.github.com>
2022-07-28 16:28:34 +1000

    jjjh

--- Control Algorithms/Navigation_Stack/start.m
+++ Control Algorithms/Navigation_Stack/start.m
@@ -1,5 +1,6 @@
 wheelBase = 0.4464; %m
 wheelRadius = 0.1715/2; %m
-
+timeStep = 0.1; %s (setting a default time step) 
+encoderRPMSwitch = 1; % default read RPM from encoders 
 
 open_system('navigation.slx')
