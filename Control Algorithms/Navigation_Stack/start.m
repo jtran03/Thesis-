e4226efd55ceb146aaebbe059aa0482acc3bb928
jtran03 <60172315+jtran03@users.noreply.github.com>
2022-07-29 13:35:52 +1000

    lolo

--- Control Algorithms/Navigation_Stack/start.m
+++ Control Algorithms/Navigation_Stack/start.m
@@ -1,10 +1,12 @@
 wheelBase = 0.4464; %m
 wheelRadius = 0.1715/2; %m
-timeStep = 0.1; % s or 10Hz  (setting a default time step) 
 
 % Kalman Filter 
-initialPose = [0, 0, 0]; 
+initialPose = [0, 0, 0]; %[x, y, theta]
+initialInput = [0, 0]; % [v, w] 
+timeStep = 0.1; % Kalman Sample Rate in s 
 initialErrorCovariance = [0.1 0 0; 0 0.1 0; 0 0 0.1]; 
+
 Q = [0.1 0 0; 0 0.1 0; 0 0 0.1]; 
 R = [0.5 0 0; 0 0.5 0; 0 0 0.5]; 
 
