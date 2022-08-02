354dd1a79aaad15145583a07a8d0065d038808e5
John Tran <j.tran03@outlook.com>
2022-08-02 23:21:59 +1000

    kiki

--- Control Algorithms/Navigation_Stack/Experiment_4_Plots/record.sh
+++ Control Algorithms/Navigation_Stack/Experiment_4_Plots/record.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-rosbag record /amr/status/voltage /amr/status/current /zlac8015d/encoder /slamware_ros_sdk_server_node/odom /imu/data /turtle1/cmd_vel
+rosbag record /amr/status/voltage /amr/status/current /zlac8015d/encoder /slamware_ros_sdk_server_node/odom /imu/data
