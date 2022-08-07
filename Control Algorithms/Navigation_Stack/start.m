f99fb46fd8c45d7baa6f7c37d52f0e70545f8773
jtran03 <60172315+jtran03@users.noreply.github.com>
2022-08-07 19:01:35 +1000

    koko

--- Control Algorithms/Navigation_Stack/start.m
+++ Control Algorithms/Navigation_Stack/start.m
@@ -1,5 +1,5 @@
 % System Parameters 
-LIVE_MODE = 0; % turn on live mapping 
+LIVE_MODE = 1; % turn on live mapping 
 wheelBase = 0.4464; %m
 wheelRadius = 0.1715/2; %m
 initialPose = [0, 0, 0]; %[x, y, theta]
@@ -15,10 +15,21 @@ encoderSampleRate = 10; %Hz
 
 
 % Transformation matrices
+% World to Odom 
+W2Ox = 0.5 - 0.0073926; 
+W2Oy = 0.5 + 0.32076;
+W2Otheta = (3/2)*pi; 
+W2OTransform = [cos(W2Otheta), -sin(W2Otheta), W2Ox; 
+                sin(W2Otheta), cos(W2Otheta), W2Oy; 
+                0            , 0            , 1]; 
+
 % Lidar to Robot Values 
-L2Rx = 0.0073926; % x
-L2Ry = -0.32076;  % y
+L2Rx = 0.32076;  % x
+L2Ry = -0.0073926; % y
 L2Rtheta = (3/2)*pi;   % theta (rad) 
+L2RTransform = [cos(L2Rtheta), -sin(L2Rtheta), L2Rx; 
+                sin(L2Rtheta), cos(L2Rtheta), L2Ry; 
+                0            , 0            , 1]; 
 
 % Kalman Filter parameters 
 xNum = 3; % Number of states
@@ -37,8 +48,6 @@ C = [1 0 0;
      0 0 1];
 D = [0 0; 
      0 0; 
-     0 0]; 
- 
-L2RTransform = [L2Rx; L2Ry; L2Rtheta];
- 
+     0 0];
+         
 open_system('navigation.slx')
